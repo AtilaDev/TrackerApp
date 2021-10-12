1bf52a8789543cc8d7eff7597f227fb366ca26e8
Leandro <atiladevelop@gmail.com>
2021-10-12 00:41:29 -0300

    Vector-Icons and React Navigation configured

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Navigator from './src/navigation/Navigator';
 
 export default function App() {
   return (
-    <View>
-      <Text>App Screen</Text>
-    </View>
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
   );
 }
---
+++ src/navigation/Navigator.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import MapScreen from '../screens/MapScreen';
+import PermissionsScreen from '../screens/PermissionsScreen';
+
+const Stack = createStackNavigator();
+
+function Navigator() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: '#fff',
+        },
+      }}>
+      <Stack.Screen name="MapScreen" component={MapScreen} />
+      <Stack.Screen name="PermissionsScreen" component={PermissionsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default Navigator;
